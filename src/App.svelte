8a4babbab8374b669900c0733a009eb91876d317
Stefano Merazzi <asso99@hotmail.com>
2022-07-27 21:22:13 +0200

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,20 @@
+<script>
+  import DriverStandings from './lib/DriverStandings.svelte'
+</script>
+
+<main>
+
+  <div class="card">
+    <DriverStandings />
+  </div>
+
+  <p class="read-the-docs">
+    Click on the Vite and Svelte logos to learn more
+  </p>
+</main>
+
+<style>
+  .read-the-docs {
+    color: #888;
+  }
+</style>
---
+++ src/lib/DriverStandings.svelte
@@ -0,0 +1,22 @@
+<script>
+  import { onMount } from 'svelte';
+
+  let standings = [];
+
+  onMount(async () => {
+    const res = await fetch(`http://ergast.com/api/f1/current/driverStandings.json`);
+    const resJson = await res.json();
+    console.log(resJson);
+    standings = resJson.MRData.StandingsTable.StandingsLists[0].DriverStandings;
+  });
+</script>
+
+<div>
+  <h1>Driver Standings</h1>
+  <ol>
+    {#each standings as driver}
+		  <li>{driver.Driver.givenName} {driver.Driver.familyName}</li>
+	  {/each}
+  </ol>
+</div>
+  
